bacfd6c955e87de584fcdf76189ec68b473253a5
joaogmontenegro <50087393+joaogmontenegro@users.noreply.github.com>
2021-06-11 03:07:04 -0300

    Alteracoes na tela Visualizar Interessados Contratante
    Add um método para que faça uma leitura do banco e visualize no listbox o retorno dos nomes do profissionais

--- TinDev/FrmVisualizarInteressadosContratante.Designer.cs
+++ TinDev/FrmVisualizarInteressadosContratante.Designer.cs
@@ -29,13 +29,25 @@ namespace TinDev
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(270, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(217, 139);
+            this.listBox1.TabIndex = 0;
+            
             // 
             // FrmVisualizarInteressadosContratante
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
             this.Name = "FrmVisualizarInteressadosContratante";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmVisualizarInteressadosDev";
@@ -44,5 +56,7 @@ namespace TinDev
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
